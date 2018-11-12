22e21e01aad2e9f7d58262d8db1dbe9ba3a03c9f
Josephfrantz92 <35854050+Josephfrantz92@users.noreply.github.com>
2018-11-12 11:59:10 -0500

    First implementation of PHP code & DB
    +Changed files to PHP
    +Added login and logout functions
    +Added session tracking
    
    -Moved files out from HonestCraig folder to root directory

--- Aquatics.php
+++ Aquatics.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> SignIn </title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title> Title </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="stylesheet.css" type="text/css" rel="stylesheet">
 </head>
 
-<body style="background-color: crimson">
-    <nav class="navbar navbar-expand-lg">
+	<body >
+<nav class="navbar navbar-expand-lg">
         <a class="navbar-brand col-9" href="#" style="text-align: center; font-size: 28pt">HonestCraig, Shopping Responsibly</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #b9bec1">
     <span class="navbar-toggler-icon"></span>
   </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -25,44 +24,13 @@
         </div>
     </nav>
     <br />
-    <div class="container col-5">
-        <h1 class="form-heading">login Form</h1>
-        <div class="login-form">
-            <div class="main-div">
-                <div class="panel">
-                    <h5>Please enter your email and password</h5>
-                </div>
-                <form id="Login">
-
-                    <div class="form-group">
-
-
-                        <input type="email" class="form-control" id="inputEmail" placeholder="Email Address">
-
-                    </div>
-
-                    <div class="form-group">
-
-                        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-
-                    </div>
-                    <div class="forgot">
-                        <a href="reset.html"><h5>Forgot Password?</h5></a>
-                    </div>
-                    <br />
-                    <button type="submit" class="btn btn-primary">Login</button>
-
-                </form>
-            </div>
-        </div>
-    </div>
-
-
-
-
+        
+        
+        
+        
+        
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
-
 </html>
